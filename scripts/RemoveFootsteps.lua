48260ee6ac005559e8fbd43ec0418a84e0250ce7
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-03-27 23:43:58 -0700

    Update RemoveFootsteps.lua
    - Made to be more concise, directly designed by the Figura community.
    - Added comments

--- scripts/RemoveFootsteps.lua
+++ scripts/RemoveFootsteps.lua
@@ -1,6 +1,15 @@
--- Remove Footsteps
-function events.on_play_sound(id, pos, _, _, _, path)
+function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
 	
-	return player:isLoaded() and (pos - player:getPos()):lengthSquared() < 1 and id:find("step") and path == "PLAYERS"
+	-- Don't trigger if the sound was played by Figura (prevent potential infinite loop)
+	if not path then return end
+	
+	-- Don't do anything if the user isn't loaded
+	if not player:isLoaded() then return end
+	
+	-- Make sure the sound is (most likely) played by the user
+	if (player:getPos() - pos):length() > 0.05 then return end
+	
+	-- If sound contains ".step", stop the actual step sound
+	if id:find(".step") then return true end
 	
 end
