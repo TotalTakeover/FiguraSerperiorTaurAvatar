5e5ab89550a2312ed4a06d9c96442e62febe88c3
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-04-04 10:52:09 -0700

    Update ActionWheel.lua
    - Overhauled
    - Added new toggles

--- scripts/ActionWheel.lua
+++ scripts/ActionWheel.lua
@@ -1,13 +1,13 @@
 -- Required scripts
-local avatar   = require("scripts.Player")
-local camera   = require("scripts.CameraControl")
-local anims    = require("scripts.Anims")
-
--- Page setups
-local mainPage      = action_wheel:newPage("MainPage")
-local avatarPage    = action_wheel:newPage("AvatarPage")
-local cameraPage    = action_wheel:newPage("CameraPage")
-local animsPage     = action_wheel:newPage("AnimationPage")
+local itemCheck = require("lib.ItemCheck")
+local avatar    = require("scripts.Player")
+local armor     = require("scripts.Armor")
+local camera    = require("scripts.CameraControl")
+local color     = require("scripts.ColorProperties")
+local fall      = require("scripts.FallSound")
+local anims     = require("scripts.Anims")
+local arms      = require("scripts.Arms")
+local pokeball  = require("scripts.Pokeball")
 
 -- Logs pages for navigation
 local navigation = {}
@@ -27,50 +27,117 @@ local function ascend()
 	
 end
 
+-- Page setups
+local pages = {
+	
+	main      = action_wheel:newPage(),
+	avatar    = action_wheel:newPage(),
+	armor     = action_wheel:newPage(),
+	camera    = action_wheel:newPage(),
+	pokemon   = action_wheel:newPage(),
+	anims     = action_wheel:newPage()
+	
+}
+
+-- Page actions
+local pageActions = {
+	
+	avatar = action_wheel:newAction()
+		:item(itemCheck("armor_stand"))
+		:onLeftClick(function() descend(pages.avatar) end),
+	
+	pokemon = action_wheel:newAction()
+		:item(itemCheck("cobblemon:leaf_stone", "dandelion"))
+		:onLeftClick(function() descend(pages.pokemon) end),
+	
+	anims = action_wheel:newAction()
+		:item(itemCheck("jukebox"))
+		:onLeftClick(function() descend(pages.anims) end),
+	
+	armor = action_wheel:newAction()
+		:item(itemCheck("iron_chestplate"))
+		:onLeftClick(function() descend(pages.armor) end),
+	
+	camera = action_wheel:newAction()
+		:item(itemCheck("redstone"))
+		:onLeftClick(function() descend(pages.camera) end)
+	
+}
+
+-- Update action page info
+function events.TICK()
+	
+	pageActions.avatar
+		:title(color.primary.."Avatar Settings")
+		:hoverColor(color.hover)
+	
+	pageActions.pokemon
+		:title(color.primary.."Pokemon Settings")
+		:hoverColor(color.hover)
+	
+	pageActions.anims
+		:title(color.primary.."Animations")
+		:hoverColor(color.hover)
+	
+	pageActions.armor
+		:title(color.primary.."Armor Settings")
+		:hoverColor(color.hover)
+	
+	pageActions.camera
+		:title(color.primary.."Camera Settings")
+		:hoverColor(color.hover)
+	
+end
+
 -- Action back to previous page
-local backPage = action_wheel:newAction()
+local backAction = action_wheel:newAction()
 	:title("§c§lGo Back?")
 	:hoverColor(vectors.hexToRGB("FF5555"))
-	:item("minecraft:barrier")
+	:item(itemCheck("barrier"))
 	:onLeftClick(function() ascend() end)
 
 -- Set starting page to main page
-action_wheel:setPage(mainPage)
+action_wheel:setPage(pages.main)
 
 -- Main actions
-mainPage
-	:action( -1,
-		action_wheel:newAction()
-			:title("§2§lAvatar Settings")
-			:hoverColor(vectors.hexToRGB("21A64C"))
-			:item("minecraft:armor_stand")
-			:onLeftClick(function() descend(avatarPage) end))
-	
-	:action( -1,
-		action_wheel:newAction()
-			:title("§2§lAnimations")
-			:hoverColor(vectors.hexToRGB("21A64C"))
-			:item("minecraft:jukebox")
-			:onLeftClick(function() descend(animsPage) end))
+pages.main
+	:action( -1, pageActions.avatar)
+	:action( -1, pageActions.pokemon)
+	:action( -1, pageActions.anims)
+	:action( -1, pokeball.togglePage)
 
 -- Avatar actions
-avatarPage
+pages.avatar
 	:action( -1, avatar.vanillaSkinPage)
 	:action( -1, avatar.modelPage)
-	:action( -1,
-		action_wheel:newAction()
-			:title("§2§lCamera Settings")
-			:hoverColor(vectors.hexToRGB("21A64C"))
-			:item("minecraft:redstone")
-			:onLeftClick(function() descend(cameraPage) end))
-	:action( -1, backPage)
+	:action( -1, pageActions.armor)
+	:action( -1, pageActions.camera)
+	:action( -1, backAction)
+
+-- Armor actions
+pages.armor
+	:action( -1, armor.allPage)
+	:action( -1, armor.bootsPage)
+	:action( -1, armor.leggingsPage)
+	:action( -1, armor.chestplatePage)
+	:action( -1, armor.helmetPage)
+	:action( -1, backAction)
 
 -- Camera actions
-cameraPage
+pages.camera
 	:action( -1, camera.posPage)
 	:action( -1, camera.eyePage)
-	:action( -1, backPage)
+	:action( -1, backAction)
+
+-- Pokemon actions
+pages.pokemon
+	:action( -1, color.shinyPage)
+	:action( -1, fall.soundPage)
+	:action( -1, backAction)
 
 -- Animation actions
-animsPage
-	:action( -1, backPage)
+pages.anims
+	:action( -1, anims.wrapPage)
+	:action( -1, anims.idlePage)
+	:action( -1, arms.movePage)
+	:action( -1, backAction)
