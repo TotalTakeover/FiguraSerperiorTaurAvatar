9786c8e9e6807a2feaca0296cc978e6ad3250907
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-04-04 10:51:46 -0700

    Create Armor.lua

---
+++ scripts/Armor.lua
@@ -0,0 +1,361 @@
+-- Required scripts
+local pokemonParts = require("lib.GroupIndex")(models.models.SerperiorTaur)
+local kattArmor    = require("lib.KattArmor")()
+local itemCheck    = require("lib.ItemCheck")
+local color        = require("scripts.ColorProperties")
+
+-- Setting the leggings to layer 1
+kattArmor.Armor.Leggings:setLayer(1)
+
+-- Armor parts
+kattArmor.Armor.Helmet
+	:addParts()
+	:addTrimParts()
+kattArmor.Armor.Chestplate
+	:addParts(
+		
+	)
+	:addTrimParts(
+		
+	)
+kattArmor.Armor.Leggings
+	:addParts(
+		
+	)
+	:addTrimParts(
+		
+	)
+kattArmor.Armor.Boots
+	:addParts(
+		
+	)
+	:addTrimParts(
+		
+	)
+
+-- Leather armor
+kattArmor.Materials.leather
+	:setTexture(textures["textures.armor.leatherArmor"])
+	:addParts(kattArmor.Armor.Helmet
+		
+	)
+	:addParts(kattArmor.Armor.Chestplate
+		
+	)
+	:addParts(kattArmor.Armor.Leggings
+		
+	)
+	:addParts(kattArmor.Armor.Boots
+		
+	)
+
+-- Chainmail armor
+kattArmor.Materials.chainmail
+	:setTexture(textures["textures.armor.chainmailArmor"])
+
+-- Iron armor
+kattArmor.Materials.iron
+	:setTexture(textures["textures.armor.ironArmor"])
+
+-- Golden armor
+kattArmor.Materials.golden
+	:setTexture(textures["textures.armor.goldenArmor"])
+
+-- Diamond armor
+kattArmor.Materials.diamond
+	:setTexture(textures["textures.armor.diamondArmor"])
+
+-- Netherite armor
+kattArmor.Materials.netherite
+	:setTexture(textures["textures.armor.netheriteArmor"])
+
+-- Turtle helmet
+kattArmor.Materials.turtle
+	:setTexture(textures["textures.armor.turtleHelmet"])
+
+-- Trims
+-- Coast
+kattArmor.TrimPatterns.coast
+	:setTexture(textures["textures.armor.trims.coastTrim"])
+
+-- Dune
+kattArmor.TrimPatterns.dune
+	:setTexture(textures["textures.armor.trims.duneTrim"])
+
+-- Eye
+kattArmor.TrimPatterns.eye
+	:setTexture(textures["textures.armor.trims.eyeTrim"])
+
+-- Host
+kattArmor.TrimPatterns.host
+	:setTexture(textures["textures.armor.trims.hostTrim"])
+
+-- Raiser
+kattArmor.TrimPatterns.raiser
+	:setTexture(textures["textures.armor.trims.raiserTrim"])
+
+-- Rib
+kattArmor.TrimPatterns.rib
+	:setTexture(textures["textures.armor.trims.ribTrim"])
+
+-- Sentry
+kattArmor.TrimPatterns.sentry
+	:setTexture(textures["textures.armor.trims.sentryTrim"])
+
+-- Shaper
+kattArmor.TrimPatterns.shaper
+	:setTexture(textures["textures.armor.trims.shaperTrim"])
+
+-- Silence
+kattArmor.TrimPatterns.silence
+	:setTexture(textures["textures.armor.trims.silenceTrim"])
+
+-- Snout
+kattArmor.TrimPatterns.snout
+	:setTexture(textures["textures.armor.trims.snoutTrim"])
+
+-- Spire
+kattArmor.TrimPatterns.spire
+	:setTexture(textures["textures.armor.trims.spireTrim"])
+
+-- Tide
+kattArmor.TrimPatterns.tide
+	:setTexture(textures["textures.armor.trims.tideTrim"])
+
+-- Vex
+kattArmor.TrimPatterns.vex
+	:setTexture(textures["textures.armor.trims.vexTrim"])
+
+-- Ward
+kattArmor.TrimPatterns.ward
+	:setTexture(textures["textures.armor.trims.wardTrim"])
+
+-- Wayfinder
+kattArmor.TrimPatterns.wayfinder
+	:setTexture(textures["textures.armor.trims.wayfinderTrim"])
+
+-- Wild
+kattArmor.TrimPatterns.wild
+	:setTexture(textures["textures.armor.trims.wildTrim"])
+
+-- Config setup
+config:name("SerperiorTaur")
+local helmet     = config:load("ArmorHelmet")
+local chestplate = config:load("ArmorChestplate")
+local leggings   = config:load("ArmorLeggings")
+local boots      = config:load("ArmorBoots")
+if helmet     == nil then helmet     = true end
+if chestplate == nil then chestplate = true end
+if leggings   == nil then leggings   = true end
+if boots      == nil then boots      = true end
+
+-- All helmet parts
+local helmetGroups = {
+	
+	
+	
+}
+
+-- All chestplate parts
+local chestplateGroups = {
+	
+	
+	
+}
+
+-- All leggings parts
+local leggingsGroups = {
+	
+	
+	
+}
+
+-- All boots parts
+local bootsGroups = {
+	
+	
+	
+}
+
+function events.TICK()
+	
+	for _, part in ipairs(helmetGroups) do
+		part:visible(helmet)
+	end
+	
+	for _, part in ipairs(chestplateGroups) do
+		part:visible(chestplate)
+	end
+	
+	for _, part in ipairs(leggingsGroups) do
+		part:visible(leggings)
+	end
+	
+	for _, part in ipairs(bootsGroups) do
+		part:visible(boots)
+	end
+	
+end
+
+-- Armor all toggle
+local function setAll(boolean)
+	
+	helmet     = boolean
+	chestplate = boolean
+	leggings   = boolean
+	boots      = boolean
+	config:save("ArmorHelmet", helmet)
+	config:save("ArmorChestplate", chestplate)
+	config:save("ArmorLeggings", leggings)
+	config:save("ArmorBoots", boots)
+	if player:isLoaded() then
+		sounds:playSound("item.armor.equip_generic", player:getPos(), 0.5)
+	end
+	
+end
+
+-- Armor helmet toggle
+local function setHelmet(boolean)
+	
+	helmet = boolean
+	config:save("ArmorHelmet", helmet)
+	if player:isLoaded() then
+		sounds:playSound("item.armor.equip_generic", player:getPos(), 0.5)
+	end
+	
+end
+
+-- Armor chestplate toggle
+local function setChestplate(boolean)
+	
+	chestplate = boolean
+	config:save("ArmorChestplate", chestplate)
+	if player:isLoaded() then
+		sounds:playSound("item.armor.equip_generic", player:getPos(), 0.5)
+	end
+	
+end
+
+-- Armor leggings toggle
+local function setLeggings(boolean)
+	
+	leggings = boolean
+	config:save("ArmorLeggings", leggings)
+	if player:isLoaded() then
+		sounds:playSound("item.armor.equip_generic", player:getPos(), 0.5)
+	end
+	
+end
+
+-- Armor boots toggle
+local function setBoots(boolean)
+	
+	boots = boolean
+	config:save("ArmorBoots", boots)
+	if player:isLoaded() then
+		sounds:playSound("item.armor.equip_generic", player:getPos(), 0.5)
+	end
+	
+end
+
+-- Sync variables
+local function syncArmor(a, b, c, d)
+	
+	helmet     = a
+	chestplate = b
+	leggings   = c
+	boots      = d
+	
+end
+
+-- Pings setup
+pings.setArmorAll        = setAll
+pings.setArmorHelmet     = setHelmet
+pings.setArmorChestplate = setChestplate
+pings.setArmorLeggings   = setLeggings
+pings.setArmorBoots      = setBoots
+pings.syncArmor          = syncArmor
+
+-- Sync on tick
+if host:isHost() then
+	function events.TICK()
+		
+		if world.getTime() % 200 == 0 then
+			pings.syncArmor(helmet, chestplate, leggings, boots)
+		end
+		
+	end
+end
+
+-- Activate actions
+setHelmet(helmet)
+setChestplate(chestplate)
+setLeggings(leggings)
+setBoots(boots)
+
+-- Setup table
+local t = {}
+
+-- Action wheel pages
+t.allPage = action_wheel:newAction()
+	:item(itemCheck("armor_stand"))
+	:toggleItem(itemCheck("netherite_chestplate"))
+	:onToggle(pings.setArmorAll)
+
+t.helmetPage = action_wheel:newAction()
+	:item(itemCheck("iron_helmet"))
+	:toggleItem(itemCheck("diamond_helmet"))
+	:onToggle(pings.setArmorHelmet)
+
+t.chestplatePage = action_wheel:newAction()
+	:item(itemCheck("iron_chestplate"))
+	:toggleItem(itemCheck("diamond_chestplate"))
+	:onToggle(pings.setArmorChestplate)
+
+t.leggingsPage = action_wheel:newAction()
+	:item(itemCheck("iron_leggings"))
+	:toggleItem(itemCheck("diamond_leggings"))
+	:onToggle(pings.setArmorLeggings)
+
+t.bootsPage = action_wheel:newAction()
+	:item(itemCheck("iron_boots"))
+	:toggleItem(itemCheck("diamond_boots"))
+	:onToggle(pings.setArmorBoots)
+
+-- Update action page info
+function events.TICK()
+	
+	t.allPage
+		:title(color.primary.."Toggle All Armor\n\n"..color.secondary.."Toggles visibility of all armor parts.")
+		:hoverColor(color.hover)
+		:toggleColor(color.active)
+		:toggled(helmet and chestplate and leggings and boots)
+	
+	t.helmetPage
+		:title(color.primary.."Toggle Helmet\n\n"..color.secondary.."Toggles visibility of helmet parts.")
+		:hoverColor(color.hover)
+		:toggleColor(color.active)
+		:toggled(helmet)
+	
+	t.chestplatePage
+		:title(color.primary.."Toggle Chestplate\n\n"..color.secondary.."Toggles visibility of chestplate parts.")
+		:hoverColor(color.hover)
+		:toggleColor(color.active)
+		:toggled(chestplate)
+	
+	t.leggingsPage
+		:title(color.primary.."Toggle Leggings\n\n"..color.secondary.."Toggles visibility of leggings parts.")
+		:hoverColor(color.hover)
+		:toggleColor(color.active)
+		:toggled(leggings)
+	
+	t.bootsPage
+		:title(color.primary.."Toggle Boots\n\n"..color.secondary.."Toggles visibility of boots.")
+		:hoverColor(color.hover)
+		:toggleColor(color.active)
+		:toggled(boots)
+	
+end
+
+-- Return action wheel pages
+return t
